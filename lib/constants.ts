5732e1084d58b9c61fec7516ff3a79f8f7994adf
ibstudioz6592 <ibstudioz6592@gmail.com>
2025-10-11 19:41:11 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,13 +1,4 @@
 import { generateDummyPassword } from "./db/utils";
 
-export const isProductionEnvironment = process.env.NODE_ENV === "production";
-export const isDevelopmentEnvironment = process.env.NODE_ENV === "development";
-export const isTestEnvironment = Boolean(
-  process.env.PLAYWRIGHT_TEST_BASE_URL ||
-    process.env.PLAYWRIGHT ||
-    process.env.CI_PLAYWRIGHT
-);
-
-export const guestRegex = /^guest-\d+$/;
-
+// This is used for timing-safe password comparison when user doesn't exist
 export const DUMMY_PASSWORD = generateDummyPassword();
